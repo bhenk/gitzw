9cca7266b3da46547cbb3e2ad58b1d0379d17892
bhenk <bhenk@telfortglasvezel.nl>
2023-06-20 13:43:58 +0200

    view by category

--- application/templates/home/creator_cat.php
+++ application/templates/home/creator_cat.php
@@ -23,7 +23,7 @@ $years = $ctrl->getCatYears();
 
 <div id="cars_container">
     <div class="car_block" id="block_view">
-        <div id="year_block">
+        <div id="year_block_left">
             <?php foreach($years as $year => $url) { ?>
             <a href="<?php echo $url; ?>">
                 <div class="year_name"><?php echo $year; ?></div>
@@ -43,8 +43,15 @@ $years = $ctrl->getCatYears();
                     </div>
                 <?php } ?>
             </div>
-            <div class="image_sub">
+            <div class="image_sub" id="subscript">
                 ----
+            </div>
+            <div id="year_block_below">
+                <?php foreach($years as $year => $url) { ?>
+                    <a href="<?php echo $url; ?>">
+                        <div class="year_name"><?php echo $year; ?></div>
+                    </a>
+                <?php } ?>
             </div>
         </div>
     </div>
@@ -62,18 +69,39 @@ $years = $ctrl->getCatYears();
     const urls = [<?php echo "'" . implode("', '", $imageData["urls"]) . "'"; ?>];
     let pointer = 0;
     const times = [2000, 3000, 6300];
+    let or_hor = false;
 
     window.addEventListener("load", function () {
-        let car = document.getElementById(id);
-        let coll = car.children;
-        coll[4].firstElementChild.firstElementChild.style.filter = "blur(20px)";
-        coll[5].firstElementChild.firstElementChild.style.filter = "blur(20px)";
-        // coll[4].firstElementChild.firstElementChild.style.opacity = "0.1";
-        // coll[5].firstElementChild.firstElementChild.style.opacity = "0,1";
+        detectOrientation();
+        if (or_hor) {
+            let car = document.getElementById(id);
+            let coll = car.children;
+            coll[4].firstElementChild.firstElementChild.style.filter = "blur(20px)";
+            coll[5].firstElementChild.firstElementChild.style.filter = "blur(20px)";
+        }
         setTimeout(loadNew, 1000);
+        const element = document.getElementById("subscript");
+        element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     });
 
+    function detectOrientation() {
+        switch (screen.orientation.type) {
+            case "landscape-primary":
+            case "landscape-secondary":
+                or_hor = true;
+                break;
+            case "portrait-secondary":
+            case "portrait-primary":
+                or_hor = false;
+                break;
+            default:
+                or_hor = false;
+        }
+
+    }
+
     function loadNew() {
+        detectOrientation();
         let image = images[pointer];
         let url = urls[pointer];
         let to = times[0];
@@ -85,8 +113,9 @@ $years = $ctrl->getCatYears();
         let img_div = document.createElement("div");
         img_div.className = "image";
         img_div.innerHTML = '<a href="' + url + '"><img src="' + image + '" alt=""></a>';
-        img_div.firstElementChild.firstElementChild.style.filter = "blur(20px)";
-        // img_div.firstElementChild.firstElementChild.style.opacity = "0.1";
+        if (or_hor) {
+            img_div.firstElementChild.firstElementChild.style.filter = "blur(20px)";
+        }
         car.appendChild(img_div);
         setTimeout(moveAndBlur, to);
     }
@@ -98,19 +127,27 @@ $years = $ctrl->getCatYears();
         car.nextElementSibling.innerText = "...";
         car.firstElementChild.style.width = "0";
         let coll = car.children;
-        coll[3].firstElementChild.firstElementChild.style.filter = "blur(20px)";
         coll[4].firstElementChild.firstElementChild.style.filter = "blur(0)";
-        // coll[3].firstElementChild.firstElementChild.style.opacity = "0.1";
-        // coll[4].firstElementChild.firstElementChild.style.opacity = "1.0";
+        if (or_hor) {
+            coll[3].firstElementChild.firstElementChild.style.filter = "blur(20px)";
+        } else {
+            for (let i = 0; i < coll.length; i++) {
+                coll[i].firstElementChild.firstElementChild.style.filter = "blur(0)";
+            }
+        }
 
         let year = years[pointer];
-        let yb = document.getElementById("year_block");
-        let ycoll = yb.children;
-        for (i=0; i < ycoll.length; i++) {
-            if (ycoll[i].firstElementChild.innerText === year) {
-                ycoll[i].firstElementChild.className = "year_accent";
+        let lb = document.getElementById("year_block_left");
+        let bb = document.getElementById("year_block_below");
+        let l_coll = lb.children;
+        let b_coll = bb.children;
+        for (let i=0; i < l_coll.length; i++) {
+            if (l_coll[i].firstElementChild.innerText === year) {
+                l_coll[i].firstElementChild.className = "year_accent";
+                b_coll[i].firstElementChild.className = "year_accent";
             } else {
-                ycoll[i].firstElementChild.className = "year_name"
+                l_coll[i].firstElementChild.className = "year_name";
+                b_coll[i].firstElementChild.className = "year_name";
             }
         }
 
